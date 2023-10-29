ca9e2d4f8d8641e0ad5605c5a44dea9c7a1d2b60
kecoaktempur <triwan98@gmail.com>
2023-10-29 17:26:32 +0700

    first-commit

--- resources/views/base/navbar.blade.php
+++ resources/views/base/navbar.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="/css/navbar.css" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -9,9 +10,17 @@
 </head>
 
 <body>
+    <img src="img/background.png">
     <navbar>
         <div class="container">
+            <img src="img/logo-nama.png">
+        </div>
     </navbar>
+    <div class="main-container">
+        <div class="landing">
+
+        </div>
+    </div>
 </body>
 
 </html>
