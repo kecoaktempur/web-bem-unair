51a4f7421b665fe8b6301ee44a5f6ada25a2f95b
kecoaktempur <triwan98@gmail.com>
2023-11-03 07:45:31 +0700

    footer patch

---
+++ resources/views/base/footer.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/ffebcdddeb.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <div class="container">
+            <div class="left-part">
+                <img src="img/logo-nama.jpg" alt="#">
+                <div class="socmed-part">
+                    <a href="" class="fa fa-instagram"></a>
+                    <a href="" class="fa fa-linkedin"></a>
+                    <a href="" class=""><i class="fa-brands fa-tiktok"></i></a>
+                    <a href="" class=""></a>
+                </div>
+            </div>
+            <div class="right-part">
+
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
